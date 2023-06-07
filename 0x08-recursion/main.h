94b3a29ba0358be44338cf91ce3225dddfefe312
Ismail <thissismail@gmail.com>
2023-06-07 15:51:25 -0700

    Add 0x08-recursion directory
    - Add _putchar, main.h, and 0-puts-recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * _putchar - prints out a character
+ * _puts_recursion - prints a string, followed by a new line.
+ * _print_rev_recursion - prints a string in reverse.
+ * _strlen_recursion - returns the length of a string.
+ * factorial - returns the factorial of a given number.
+ * wildcmp - compares two strings and returns 1 if the strings can be considered identical
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
+ * is_prime_number - returns 1 if the input integer is a prime number, otherwise return 0.
+ * _sqrt_recursion - returns the natural square root of a number.
+ * _pow_recursion - returns the value of x raised to the power of y.
+ */
+
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int _putchar(char);
+int wildcmp(char *s1, char *s2);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int factorial(int n);
+int _strlen_recursion(char *s);
