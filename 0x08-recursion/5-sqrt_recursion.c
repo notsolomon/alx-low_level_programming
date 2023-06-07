e55ef943e4bb8eb464b27621f78bac0ee77b1d4a
Ismail <thissismail@gmail.com>
2023-06-07 16:58:16 -0700

    Add function to find sqr root of n

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,34 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - calculates natural square root of a number
+ * sqr_root - Fidn sqr root of n
  * @n: number
- *
- * Return: natural squareroot of n
+ * @root: number
+ * Return: Natural sqrt
  */
 
-int _pow_recursion(int x, int y)
+int sqr_root(int n, int root)
 {
-	if (y < 0)
+	if (root * root < n)
 		return (-1);
-	if (y == 0)
-		return (1);
+	if (root * root == n)
+		return (root);
 
-	return (_pow_recursion(x, (y - 1)) * x);
+	return (sqr_root(n, root + 1));
 }
+
+/**
+ * _sqrt_recursion - returns natural sqr root of a number
+ * @n: int
+ *
+ * Return: sqr root of n
+ */
+
+int _sqr_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (sqr__root(n, 0));
+}
+/**
