7922ff4deb3a0c1c02109247083dc5e5675dc3b0
Ismail <thissismail@gmail.com>
2023-06-07 17:42:03 -0700

    Fix betty styles

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -43,7 +43,7 @@ int wildcmp(char *s1, char *s2)
 		return (1);
 
 	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 +1));
+		return (wildcmp(s1 + 1, s2 + 1));
 	else if (*s2 == '*')
 		return (match_substr(s1, (s2 + 1), (s2 + 1)));
 	else
