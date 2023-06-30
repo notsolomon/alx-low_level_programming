104722e8a7b94a31a2be7b81465a99644b13c384
Ismail <thissismail@gmail.com>
2023-06-30 15:39:20 -0700

    Add lists.h

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,40 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/**
+ * _putchar - output characters
+ * print_list - prints all elements of a list
+ * list_len - returns the number of elements in a list
+ * add_node_end - adds a node at the end of a list
+ * list_t - adds a new node at the beginning of a list_l
+ * free_list - frees a list
+ */
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
