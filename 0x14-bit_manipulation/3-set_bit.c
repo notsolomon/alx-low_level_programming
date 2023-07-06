d3983acbd5756791b63fe45bf1068348ccae9017
Ismail <thissismail@gmail.com>
2023-07-06 16:56:54 -0700

    Add set_bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * set_bit - set bit to 1 at given index
+ * @n: number
+ * @index: index within binary number
+ *
+ * Return: 1 if success, -1 otherwise
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int max_bits;
+	unsigned long int mask = 1;
+
+	max_bits = (sizeof(unsigned long int) * 8);
+	if (index > max_bits)
+		return (-1);
+
+	mask <<= index;
+	*n = (*n | mask);
+
+	return (1);
+}
