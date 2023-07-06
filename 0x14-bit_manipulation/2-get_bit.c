d35ee11ff050a8cca0fd919ad8388d609af6dce3
Ismail <thissismail@gmail.com>
2023-07-06 16:48:50 -0700

    Add get_bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_bit - get bit at index
+ * @n: number
+ * @index: index within binary number
+ * Return: bit 0 or 1, or -1 if error
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int max_bits;
+	int bit;
+
+	max_bits = (sizeof(unsigned long int) * 8);
+	if (index > max_bits)
+		return (-1);
+
+	bit = ((n >> index) & 1);
+
+	return (bit);
+}
