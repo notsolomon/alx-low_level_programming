35cb6d65d08f0dda99951b9400f64819805ef8ab
Ismail <thissismail@gmail.com>
2023-07-06 17:00:33 -0700

    Add clear_bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,24 @@
+nclude "main.h"
+
+/**
+ * clear_bit - clear bit to 0 at given index
+ * @n: number
+ * @index: index
+ *
+ * Return: 1 if success-1 otherwise
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int max_bits;
+	unsigned long int mask = 1;
+
+	max_bits = (sizeof(unsigned long int) * 8);
+	if (index > max_bits)
+		return (-1);
+
+	mask = ~(mask << index);
+	*n = (*n & mask);
+
+	return (1);
+}
